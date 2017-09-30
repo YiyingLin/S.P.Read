fa6998efc741eaf6a3b4d98b9e00151a8d9b2677
Chen Chen <chenchen_930816@alumni.ubc.ca>
2017-09-30 15:58:57 -0700

    landing page

--- index.vr.js
+++ index.vr.js
@@ -5,41 +5,118 @@ import {
   Pano,
   Text,
   View,
+  Plane,
 } from 'react-vr';
 
 export default class Bread extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      passage: 'Then our mother came in,And she said to us two,“Did you have any fun?Tell me. What did you do?”And Sally and I did not know what to say.Should we tell her, The things that went on, there that day? Well... what would YOU do, If your mother asked you?'.split(' '),
-      indexToRender: 0
+      summary1: null,
+      summary2: null,
+      summary3: null
     };
-
-    setInterval(() => {
-      this.setState(previousState => {
-        return { indexToRender: this.state.indexToRender + 1 };
-      });
-    }, 250);
   }
 
+  showSummary1 = () => {
+    this.setState({
+      summary1: (<Plane
+          dimWidth={1}
+          dimDepth={2}
+          style={{
+            color: 'red',
+            transform: [{translate: [1.3, 0, -0.88]}, {rotateY: -60}, {scale: 0.6}],
+          }}
+      />)
+    })
+  };
+
+  hideSummary1 = () => {
+    this.setState({
+      summary1: null
+    })
+  };
+
+  showSummary2 = () => {
+    this.setState({
+      summary2: (<Plane
+          dimWidth={1}
+          dimDepth={2}
+          style={{
+            color: 'red',
+            transform: [{translate: [-1.3, 0, -0.88]}, {rotateY: 60}, {scale: 0.6}],
+          }}
+      />)
+    })
+  };
+
+  hideSummary2 = () => {
+    this.setState({
+      summary2: null
+    })
+  };
+
+  showSummary3 = () => {
+    this.setState({
+      summary3: (<Plane
+          dimWidth={1}
+          dimDepth={2}
+          style={{
+            color: 'red',
+            transform: [{translate: [1.3, 0, 0.88]}, {rotateY: -120}, {scale: 0.6}],
+          }}
+      />)
+    })
+  };
+
+  hideSummary3 = () => {
+    this.setState({
+      summary3: null
+    })
+  };
+
   render() {
+    const summary1 = this.state.summary1;
+    const summary2 = this.state.summary2;
+    const summary3 = this.state.summary3;
+
     return (
       <View>
-        <Pano source={asset('chess-world.jpg')}/>
-        <Text
+
+        <Pano source={asset('library.jpg')}/>
+        <Plane
+          dimWidth={0.5}
+          dimDepth={1}
           style={{
-            backgroundColor: '#777879',
-            fontSize: 0.8,
-            fontWeight: '400',
-            layoutOrigin: [0.5, 0.5],
-            paddingLeft: 0.2,
-            paddingRight: 0.2,
-            textAlign: 'center',
-            textAlignVertical: 'center',
-            transform: [{translate: [0, 0, -3]}],
-          }}>
-          {this.state.passage[this.state.indexToRender]}
-        </Text>
+            color: 'red',
+            transform: [{translate: [1.2, -0.7, -1]}, {rotateY: -90}, {rotateX: -90}, {scale: 0.6}],
+          }}
+          onEnter={() => this.showSummary1()}
+          onExit={() => this.hideSummary1()}
+        />
+        <Plane
+          dimWidth={0.5}
+          dimDepth={1}
+          style={{
+            color: 'red',
+            transform: [{translate: [-1.8, -0.7, -1.2]}, {rotateY: -90}, {rotateX: -90}, {scale: 0.6}],
+          }}
+          onEnter={() => this.showSummary2()}
+          onExit={() => this.hideSummary2()}
+        />
+        <Plane
+          dimWidth={0.5}
+          dimDepth={1}
+          style={{
+            color: 'red',
+            transform: [{translate: [2.1, -0.7, 1.2]}, {rotateY: -90}, {rotateX: -90}, {scale: 0.6}],
+          }}
+          onEnter={() => this.showSummary3()}
+          onExit={() => this.hideSummary3()}
+        />
+        { summary1 }
+        { summary2 }
+        { summary3 }
       </View>
     );
   }
