76d0a8a2cc9c2d3363061f6f95ac15eb715d7730
YiyingLin <lin.catherine831@gmail.com>
2017-09-30 19:49:57 -0700

    Pause function

--- reading.js
+++ reading.js
@@ -7,6 +7,7 @@ import {
   Pano,
   Text,
   View,
+  VrButton,
 } from 'react-vr';
 
 const PASSAGE = 'Then our mother came in, And she said to us two, “Did you have any fun? Tell me. What did you do? ”And Sally and I did not know what to say. Should we tell her, The things that went on, there that day? Well... what would YOU do, If your mother asked you? Then our mother came in, And she said to us two, “Did you have any fun? Tell me. What did you do? ”And Sally and I did not know what to say. Should we tell her, The things that went on, there that day? Well... what would YOU do, If your mother asked you? Then our mother came in, And she said to us two, “Did you have any fun? Tell me. What did you do? ”And Sally and I did not know what to say. Should we tell her, The things that went on, there that day? Well... what would YOU do, If your mother asked you?';
@@ -19,7 +20,9 @@ export default class Reading extends React.Component {
       displayWord: "",
       xPosition: '',
       yPosiitons: -1,
-      interval: 1000
+      interval: 1000,
+      previousInterval: '',
+      pauseInterval: 9999999999999
     };
 
     this.timer = setInterval(() => {
@@ -32,7 +35,6 @@ export default class Reading extends React.Component {
   }
 
   deconstructEvent = (nativeEvent) => {
-    console.log('haha')
     switch (nativeEvent.inputEvent.eventType) {
       case "mousemove":
         if (this.state.xPosition ==='') {
@@ -40,7 +42,7 @@ export default class Reading extends React.Component {
         } else if (this.state.xPosition < nativeEvent.inputEvent.viewportX) {
           this.setState({xPosition: nativeEvent.inputEvent.viewportX}, () => {
             if (this.state.interval > 100) {
-              this.setState({interval: this.state.interval - 5}, () => {
+              this.setState({interval: this.state.interval - 10}, () => {
                 clearInterval(this.timer)
                 this.timer = setInterval(() => {
                   this.setState(previousState => {
@@ -53,7 +55,7 @@ export default class Reading extends React.Component {
         } else if (this.state.xPosition > nativeEvent.inputEvent.viewportX) {
           this.setState({xPosition: nativeEvent.inputEvent.viewportX}, () => {
             if (this.state.interval < 1000) {
-              this.setState({interval: this.state.interval + 5}, () => {
+              this.setState({interval: this.state.interval + 10}, () => {
                 clearInterval(this.timer)
                 this.timer = setInterval(() => {
                   this.setState(previousState => {
@@ -72,6 +74,37 @@ export default class Reading extends React.Component {
     }
   }
 
+  pauseToggle = (event) => {
+    console.log(this.state.interval)
+    if (this.state.interval === 9999999999999) {
+      this.setState({interval: this.state.previousInterval}, () => {
+        this.setState({previousInterval: 0}, () => {
+          clearInterval(this.timer)
+          this.timer = setInterval(() => {
+            this.setState(previousState => {
+              return {
+                displayWord: this.state.parser.nextState()
+               };
+            });
+          }, this.state.interval);
+        })
+      })
+    } else {
+      this.setState({previousInterval: this.state.interval}, () => {
+        this.setState({interval: this.state.pauseInterval}, () => {
+          clearInterval(this.timer)
+          this.timer = setInterval(() => {
+            this.setState(previousState => {
+              return {
+                displayWord: this.state.parser.nextState()
+               };
+            });
+          }, this.state.interval);
+        })
+      })
+    }
+  }
+
   render() {
     const angleX = this.props.angleX;
     const angleY = this.props.angleY;
@@ -82,20 +115,27 @@ export default class Reading extends React.Component {
         onInput={(event) => this.deconstructEvent(event.nativeEvent)}
         >
         <Pano source={asset('chess-world.jpg')}/>
-        <Text
+        <VrButton
+          onClick={() => this.pauseToggle()}
           style={{
-            backgroundColor: 'transparent',
-            fontSize: 0.8,
-            fontWeight: '400',
-            layoutOrigin: [0.5, 0.5],
-            paddingLeft: 0.2,
-            paddingRight: 0.2,
-            textAlign: 'center',
-            textAlignVertical: 'center',
-            transform: [{rotateX: angleX}, {rotateY: angleY}, {rotateZ: angleZ}, {translate: [0, 0, -3]}],
-          }}>
-          {this.state.displayWord}
-        </Text>
+            backgroundColor: 'red',
+          }}
+          >
+          <Text
+            style={{
+              backgroundColor: 'transparent',
+              fontSize: 0.8,
+              fontWeight: '400',
+              layoutOrigin: [0.5, 0.5],
+              paddingLeft: 0.2,
+              paddingRight: 0.2,
+              textAlign: 'center',
+              textAlignVertical: 'center',
+              transform: [{rotateX: angleX}, {rotateY: angleY}, {rotateZ: angleZ}, {translate: [0, 0, -3]}],
+            }}>
+            {this.state.displayWord}
+          </Text>
+        </VrButton>
       </View>
     );
   }
